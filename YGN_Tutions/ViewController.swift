088ac81c536fcb4d7ff9b7b4e98846e7c5ac9278
Timmy Tseng <timmytseng@timmy-tseng.local>
2018-06-30 15:32:16 +0630

    New Commit

--- YGN_Tutions/ViewController.swift
+++ YGN_Tutions/ViewController.swift
@@ -44,14 +44,14 @@ class ViewController: UIViewController {
         
     }
 //    func customizeNavBar() {
-//        
-//        
+//
+//
 //        navigationController?.navigationBar.tintColor = UIColor(colorLiteralRed: 255/255, green: 255/255, blue: 255/255, alpha: 1)
 //        navigationController?.navigationBar.barTintColor = UIColor(colorLiteralRed: 255/255, green: 87/255, blue: 35/255, alpha: 1)
-//        
+//
 //        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
-//        
-//        
+//
+//
 //    }
     
     /*
