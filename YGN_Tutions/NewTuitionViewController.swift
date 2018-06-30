344005dd3b7d95a9f455e8e8912bf1efdfda64b9
Timmy Tseng <timmytseng@timmy-tseng.local>
2018-06-30 16:07:41 +0630

    Embeded ViewControllers

---
+++ YGN_Tutions/NewTuitionViewController.swift
@@ -0,0 +1,57 @@
+//
+//  NewTuitionViewController.swift
+//  YGN_Tuitions
+//
+//  Created by Timmy Tseng on 6/30/18.
+//  Copyright © 2018 Timmy Tseng. All rights reserved.
+//
+
+import UIKit
+
+class NewTuitionViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        sideMenus()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func sideMenus() {
+        
+        if revealViewController() != nil {
+            
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 0 //160
+            
+            
+            //alertButton.target = revealViewController()
+            //alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
