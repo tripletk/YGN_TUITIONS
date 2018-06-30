4336c9cbf5d72745b5f92dee6727ce55e67578b8
Timmy Tseng <timmytseng@timmy-tseng.local>
2018-06-30 12:11:34 +0630

    Initial Commit

---
+++ YGN_Tutions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YGN_Tutions
+//
+//  Created by Timmy Tseng on 6/30/18.
+//  Copyright © 2018 Timmy Tseng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
