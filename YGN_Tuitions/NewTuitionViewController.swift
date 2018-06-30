1463cbcc3ef90df49047e7863931273026d36bda
Timmy Tseng <timmytseng@timmy-tseng.local>
2018-06-30 17:30:58 +0630

    fixed again again

--- YGN_Tuitions/NewTuitionViewController.swift
+++ YGN_Tuitions/NewTuitionViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class NewTuitionViewController: UIViewController {
 
     
+  
     @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
