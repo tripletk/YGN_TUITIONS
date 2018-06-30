1e87283da87df946825ee043158534bbed87db50
Timmy Tseng <timmytseng@timmy-tseng.local>
2018-06-30 15:18:09 +0630

    Refurbished SideMenu

--- YGN_Tutions/ViewController.swift
+++ YGN_Tutions/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  YGN_Tutions
+//  YGN_Tuitions
 //
 //  Created by Timmy Tseng on 6/30/18.
 //  Copyright © 2018 Timmy Tseng. All rights reserved.
@@ -8,21 +8,60 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
-        // Do any additional setup after loading the view, typically from a nib.
+        sideMenus()
+        
+        
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func sideMenus() {
+        
+        if revealViewController() != nil {
+            
+            menuButton.target = revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController().rearViewRevealWidth = 275
+            revealViewController().rightViewRevealWidth = 0 //160
+            
+            
+            //alertButton.target = revealViewController()
+            //alertButton.action = #selector(SWRevealViewController.rightRevealToggle(_:))
+            
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+    }
+    func customizeNavBar() {
+        
+        
+        navigationController?.navigationBar.tintColor = UIColor(colorLiteralRed: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        navigationController?.navigationBar.barTintColor = UIColor(colorLiteralRed: 255/255, green: 87/255, blue: 35/255, alpha: 1)
+        
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
